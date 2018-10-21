b5e6f10e7ddb8faa2e6c66d13a366be9d6607c0d
MSI\KuraiKun <1992kurai@gmail.com>
2018-10-21 13:47:19 +0100

    First Commit
    -Made initial folders
    -Import basic standard assets
    -Made camera basic switch between FPS/TPS Mode
    -Basic notes for the GUI
    -Working on the Smooth follow and advanced free look camera if switched between FPS/TPS etc ( ledd given assets and prefabs used, more personalized/detailler/custumizatzad classs for ....)

---
+++ Assets/Scripts/Players/Local/SwitchCameras.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchCameras : MonoBehaviour {
+
+    //Attributes
+    [SerializeField] Camera charCam;
+    [SerializeField] Transform FPSTrans;
+    [SerializeField] Transform TPSTrans;
+    [SerializeField] GameObject smoothFollow;
+
+    //Functions
+
+	// Use this for initialization
+	void Start ()
+    {
+        
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //Change Camera while pressing mouse Button 1/2 to FPS/TPS while playing!
+        if (Input.GetMouseButtonDown(1))
+        {
+            charCam.transform.position = FPSTrans.transform.position;
+            charCam.transform.rotation = FPSTrans.transform.rotation;
+            smoothFollow.SetActive(false);
+        }
+        //Change Camera while pressing mouse Button 1/2 to FPS/TPS while playing!
+        if (Input.GetMouseButtonDown(2))
+        {
+            charCam.transform.position = TPSTrans.transform.position;
+            charCam.transform.rotation = TPSTrans.transform.rotation;
+            smoothFollow.SetActive(true);
+        }
+
+    }
+}
